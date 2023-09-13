ecd56347bcbbf3f319eba5d715385b862ef24136
butterincup <143638043+butterincup@users.noreply.github.com>
2023-09-13 16:54:43 -0700

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/eva1_9/EVA1_9.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_9;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int 1nombre; nombre no es valido, empiesa con un numero
+        int $nombre;
+        int _nombre;
+        int nombre1;
+        int Nombre;
+        int nombre;
+        int nombre_dato;
+        //no podemos usar la palabra public, es una palñabra reservada
+        int Public;
+                
+    }
+    
+}
